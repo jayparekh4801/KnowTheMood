e8a0e7efc99d8ffc29734f3fa76e8f77e916b87d
jayparekh4801 <parekhjay4801@gmail.com>
2021-06-17 06:39:47 +0530

    libraries added

---
+++ train.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+
+from tensorflow import keras
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
