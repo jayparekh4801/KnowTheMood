0fd2ae33184756d779319cec1c3de01bcb546fa3
jayparekh4801 <parekhjay4801@gmail.com>
2021-06-18 10:49:15 +0530

    some changes

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D
-from keras.optimizers import Adam
+from keras.optimizers import adamax_v2
 from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -21,7 +21,7 @@ training_set = train_datagen.flow_from_directory(
                                                 train_image_dir,
                                                 target_size = (48, 48),
                                                 batch_size = 64,
-                                                color_mode = "gray_framescale",
+                                                color_mode = "grayscale",
                                                 class_mode = "categorical"
                                                 )
 
@@ -29,7 +29,7 @@ test_set = test_datagen.flow_from_directory(
                                             test_image_dir,
                                             target_size = (48, 48),
                                             batch_size = 64,
-                                            color_mode = "gray_framescale",
+                                            color_mode = "grayscale",
                                             class_mode = "categorical"
                                             )
 
@@ -38,8 +38,8 @@ test_set = test_datagen.flow_from_directory(
 ################################### Building Convolutional Neural Network ####################
 
 emotion_model = Sequential()
-emotion_model.add(Conv2D(32, (3, 3), activation='relu', inputshape = (48, 48, 1)))
-emotion_model.add(Conv2D(32, (3, 3), activation='relu', inputshape = (48, 48, 1)))
+emotion_model.add(Conv2D(32, (3, 3), activation='relu', input_shape = [48, 48, 1]))
+emotion_model.add(Conv2D(32, (3, 3), activation='relu', input_shape = [48, 48, 1]))
 emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
 emotion_model.add(Dropout(0.25))
 
@@ -58,7 +58,7 @@ emotion_model.add(Dense(7, activation="softmax"))
 
 #################################### Compiling CNN ############################################
 
-emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+emotion_model.compile(loss='categorical_crossentropy', optimizer=adamax_v2(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
 emotion_detector = emotion_model.fit_generator(
                                                 training_set,
