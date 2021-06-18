487839a869ad2409a13e172a4b63575f32c8c911
jayparekh4801 <parekhjay4801@gmail.com>
2021-06-18 10:34:52 +0530

    CNN Compiled

--- train.py
+++ train.py
@@ -38,5 +38,35 @@ test_set = test_datagen.flow_from_directory(
 ################################### Building Convolutional Neural Network ####################
 
 emotion_model = Sequential()
+emotion_model.add(Conv2D(32, (3, 3), activation='relu', inputshape = (48, 48, 1)))
+emotion_model.add(Conv2D(32, (3, 3), activation='relu', inputshape = (48, 48, 1)))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation="relu"))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation="softmax"))
+
+###############################################################################################
+
+#################################### Compiling CNN ############################################
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+
+emotion_detector = emotion_model.fit_generator(
+                                                training_set,
+                                                steps_per_epoch=28709,
+                                                epochs=50,
+                                                validation_data=test_set,
+                                                validation_steps=7178
+)
 
 
+                                                
