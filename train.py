60608b07b95477ef1a570e0b834e6b677f732aa8
jayparekh4801 <parekhjay4801@gmail.com>
2021-06-24 08:00:07 +0530

    image predicted

--- train.py
+++ train.py
@@ -104,9 +104,14 @@ while(True) :
     num_faces = bounding_box.detectMultipleScale(gray_frame, scale_factor = 1.3, minNeighbors = 5)
 
     for (x, y, w, h) in num_faces :
-        cv2.Rec
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y : y + h, x : x + w]
+        cropped_image = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        emotion_prediction = emotion_model.predict(cropped_image)
+        maxindex = int(np.argmax(emotion_prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
 
 
 
-                                                
+                                                 
